faab641ffa73fc96618c07c9c85edddc6cf046af
Nico Jeske <nico.jeske@gmail.com>
2024-11-16 02:17:11 +0100

    feat: :sparkles: Replace the nextjs logo with a title

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,9 @@ export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-8 sm:p-20">
             <main className="w-full max-w-5xl flex flex-col gap-8 items-center sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
+                <h1 className="text-4xl font-bold text-center sm:text-left">
+                   Willkommen zum <span className="text-blue-500">Heiligen Rad</span>
+                </h1>
                 <AnimeList />
             </main>
         </div>
