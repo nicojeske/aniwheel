50720e6b58e7fd9a7a1222da6fc4e8888b115dfe
Nico Jeske <nico.jeske@gmail.com>
2024-11-20 05:53:54 +0100

    style: :lipstick: Increase max width on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
             <div className="flex flex-col flex-grow items-center justify-center bg-gray-900 text-white p-8 sm:p-20">
-                <main className="w-full max-w-5xl flex flex-col gap-8 items-center sm:items-start">
+                <main className="w-full max-w-screen-2xl flex flex-col gap-8 items-center sm:items-start">
                     <h1 className="text-4xl font-bold text-center sm:text-left">
                         {t('title')} <span className="text-blue-500">{t('title_highlight')}</span>
                     </h1>
